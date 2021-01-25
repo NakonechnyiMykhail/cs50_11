624c97364e290c65128f35dd5526419e78242509
mikhail <mykhail.nakonechnyi@gmail.com>
2021-01-25 12:07:00 +0000

    python lesson 2

---
+++ py/arrays.py
@@ -0,0 +1,64 @@
+my_list = []
+my_list2 = [1,3,5,7,9,0,2,4,5,8]
+my_str_list = ["Mike", "Ivan", "Denis"]
+# print(my_str_list[1])
+# for index in range(5):
+#     print(my_list2[index])
+# range(10) = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+# index = 0
+# index = 1
+# ...
+# index = 9
+
+# for index in range(10):
+#     print(index)
+
+# range(10, 20) = [10, 11, 12, 13, 14, 15, 16, 17, 18, 19]
+# index = 10
+# index = 11
+# ...
+# index = 19
+
+# for index in range(10, 20):
+#     print(index)
+
+# range(10, 50, 5) = [10, 15, 20, 25, 30, 35, 40, 45]
+# index = 10
+# index = 11
+# ...
+# index = 19
+
+# for index in range(10, 50, 5):
+#     print(index)
+
+
+# for index in my_list2:
+#     print(index)
+
+my_dict0 = {}
+my_dict_1 = {
+    "9class": 7,
+    "10class": 6,
+    "11class": 7
+}
+#print(len(my_dict_1)) # length of list and dict
+# for index in range(5):
+#     print(my_list2[index])
+
+thisdict =	{
+  "brand": "Ford",
+  "electric": False,
+  "year": 1964,
+  "colors": ["red", "white", "blue"],
+  "new_dict" : {
+        "9class": 7,
+        "10class": 6,
+        "11class": 7,
+    },
+    2020: "last year"
+}
+print(thisdict[2020]) # "last year"
+
+print(thisdict.items())
+print(thisdict.keys())
+print(thisdict.values())
--- py/calc.py
+++ py/calc.py
@@ -1,14 +1,14 @@
-from cs50 import get_int, get_char
-num1 = get_int("Number1: ")
-num2 = get_int("Number2: ")
-oper = get_char("Oper: ")
+from cs50 import get_float, get_string
+num1 = get_float("Number1: ")
+num2 = get_float("Number2: ")
+oper = get_string("Oper: ")
 if "+" == oper:
     print(f"{num1} + {num2} =", num1+num2)
 elif "-" == oper:
-    # ...
+    print(f"{num1} + {num2} =", num1+num2)
 elif "/" == oper:
-    #...
+    print(f"{num1} + {num2} =", num1+num2)
 elif "*" == oper:
-    # ...
+    print(f"{num1} + {num2} =", num1+num2)
 else:
     print("smth else")
